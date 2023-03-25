c0ed833d0e5b025cc7c134bf78e80ab3b80f632f
Landin Kasti <lkastiman@gmail.com>
2023-03-25 17:32:17 -0500

    added other filter option

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -10,7 +10,7 @@ const FilterBox = (props) => {
                 <optgroup label='Gender'>
                     <option value="male">male</option>
                     <option value="female">female</option>
-                {/* <option value="other">other</option> */}
+                    <option value="other">other</option>
                 </optgroup>
                 <optgroup label='Species'>
                     <option value="droid">droid</option>
